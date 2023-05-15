98d3de7752a649b8dfab945531fdb3fa680d5cf9
guhandelta <guhan.delta@gmail.com>
2023-05-14 23:15:30 -0400

    Created an endpoint to get the prompts from db and populated the Feeds section through PromptCards

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+"use client";
+
+import { useState, useEffect } from 'react'
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map(post => (
+        <PromptCard 
+          key={post._id}  
+          post={post}  
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
+
 
 const Feed = () => {
+
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  // Fetching all the posts on initial page load
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setPosts(data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+
+  const handleSearchChange = e =>{
+
+  }
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex center">
+        <input 
+          type="text" 
+          value={searchText} 
+          placeholder="Search for a tag or username" 
+          onChange={handleSearchChange}
+          required	
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList
+        data={posts}
+        handleTagClick={() => {}}
+      />
+    </section>
   )
 }
 
